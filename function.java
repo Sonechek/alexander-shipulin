0587cf6d0da2aacf00e45ca37f33fc226b17c1ab
Alexander Shipulin <71063702+Sonechek@users.noreply.github.com>
2025-01-15 21:33:09 +0500

    changed start point

--- function.java
+++ function.java
@@ -1,14 +1,14 @@
 public class function {
 
     public static void main(String[] args) {
-        double start_point = -2.6;
+        double start_1 = -2.6;
         double end = 3.7;
         double step = 0.3;
 
         System.out.println("Таблица значений функций:");
         System.out.printf("%10s %15s %15s %15s\n", "x", "f1(x)", "f2(x)", "f3(x)");
         System.out.println("-------------------------------------------------------------------");
-        for (double x = start_point; x <= end; x += step) {
+        for (double x = start_1; x <= end; x += step) {
             double f1 = Math.pow(x, 5) * (1 / Math.tan(2 * Math.pow(x, 3)));
             double f2 = 5 / (Math.tan(2 * x + 3) + 1);
             double f3 = Math.pow(x, 2) * Math.exp(-x);
